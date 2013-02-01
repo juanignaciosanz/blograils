065f69b72b68dd7cc4f9ca8c594cb7e996d0294d
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2013-02-01 01:29:19 +0100

    Comments and security added
    Comment model added with CD operations so relationships and
    dependencies with post added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-  validates :name,  :presence => true
-  validates :title, :presence => true,
-            :length => { :minimum => 5 }
+	attr_accessible :content, :name, :title
+	validates :name,  :presence => true
+	validates :title, :presence => true, :length => { :minimum => 5 }
+	has_many :comments, :dependent => :destroy	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Blog::Application.routes.draw do
   
   get "home/index"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
